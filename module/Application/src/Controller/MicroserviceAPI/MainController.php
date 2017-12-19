0a35c6157b4b10db542783fe3b38b4407aea1b61
Run Away <runaway90@zoho.com>
2017-12-19 14:28:37 +0100

    Add API Part

---
+++ module/Application/src/Controller/MicroserviceAPI/MainController.php
@@ -0,0 +1,20 @@
+<?php
+namespace Controller\MicroserviceAPI;
+
+use Zend\Mvc\Controller\AbstractRestfulController,
+    Zend\View\Model\JsonModel;
+
+class MainController extends AbstractRestfulController
+{
+    public function apiAction()
+    {
+        $em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        $testEntity = $em->getRepository('v1\Entity\Test')->findAll();
+
+        foreach ($testEntity as $entity) {
+           $array[] = $entity->getJsonData();
+        }
+
+        return new JsonModel(array('response' => $array));
+    }
+}
